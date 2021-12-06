796a61b5b468d0838c7d8392faff4c13b0613bf7
shaunsingh <shaunsingh0207@gmail.com>
2021-12-06 15:04:03 -0500

    (nix) fix formatting

--- shell.nix
+++ shell.nix
@@ -1,19 +1,14 @@
 { pkgs ? import <nixpkgs> {
-    overlays = [
-      (import (builtins.fetchTarball {
-        url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
-      }))
-    ];
-  }
-}:
+  overlays = [
+    (import (builtins.fetchTarball {
+      url =
+        "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
+    }))
+  ];
+} }:
 with pkgs;
 mkShell {
-  buildInputs = [
-    neovim-nightly
-    luajit
-    ripgrep
-    nodejs
-  ];
+  buildInputs = [ neovim-nightly luajit ripgrep nodejs ];
 
   shellHook = ''
     alias nvim="nvim -u $(pwd)/init.lua"
